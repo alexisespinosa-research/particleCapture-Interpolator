bff3335995042e607c72ca59c5b2ebc129c602e0
AlexisEspinosaGayosso <Alexis.Espinosa@pawsey.org.au>
2020-08-21 13:20:34 +0800

    Commiting was was left from the previous sprint

---
+++ printTables.py
@@ -0,0 +1,33 @@
+import numpy as np
+import csv
+
+#The capture efficiency function
+def ceff(RE,rp):
+    eta=0.224*(RE**0.718)*(rp**2.08)
+    return eta
+
+#The rp's to ask
+rps=np.arange(0.0,0.01,0.001,dtype=float)
+print(rps,rps.shape)
+
+#The Re's to ask
+REs=np.array([100,200,300,400,500,600,700,800,900,1000],dtype=float,ndmin=1)
+print(REs,REs.shape)
+
+#Directory to save the tables
+tablesDir="testTables"
+
+#Generating the tables
+for REi in REs:
+    fileName=tablesDir+"/"+"ceff_"+str(REi)+".csv"
+    myData=np.empty((rps.size,2),dtype=float)
+    print(myData,myData.shape,myData.size)
+    for row in range(0,rps.size):        
+        myData[row][0]=rps[row]
+        myData[row][1]=ceff(REi,rps[row])
+        print(myData[row][:])
+    myFile = open(fileName, 'w')
+    with myFile:
+        writer = csv.writer(myFile)
+        writer.writerows(myData)
+
